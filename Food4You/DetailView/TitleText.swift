e8c776ed8f1e5802a17d858a23690d7b25b5f74a
Ariya Ghomashtchi <ariya_ghomashtchi@yahoo.com>
2023-05-25 13:31:07 +0200

    UIFile

---
+++ Food4You/DetailView/TitleText.swift
@@ -0,0 +1,21 @@
+//
+//  TitleText.swift
+//  Food4You
+//
+//  Created by Ariya Ghomashtchi on 25.05.23.
+//
+
+import SwiftUI
+
+struct TitleText: View {
+    var body: some View {
+        Text("Hello Isack")
+            .bold()
+    }
+}
+
+struct TitleText_Previews: PreviewProvider {
+    static var previews: some View {
+        TitleText()
+    }
+}
