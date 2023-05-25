6ab9572a605013a616ad7e4714195f5d5a3f48cf
Ariya Ghomashtchi <ariya_ghomashtchi@yahoo.com>
2023-05-25 10:43:49 +0200

    Initial Commit

---
+++ Food4You/Food4YouApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food4YouApp.swift
+//  Food4You
+//
+//  Created by Ariya Ghomashtchi on 25.05.23.
+//
+
+import SwiftUI
+
+@main
+struct Food4YouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
