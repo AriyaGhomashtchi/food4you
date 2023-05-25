77d0293b953bfdff8e17515c1e6cbb867ce398ef
IsackHaer <isack.h@yahoo.com>
2023-05-25 11:02:05 +0200

    more Text has been added

--- Food4You/ContentView.swift
+++ Food4You/ContentView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Isack was here!!!!!!")
+            Text("Isack was here again 4")
+            Text("Isack was here again 4")
+            Text("Isack was here again 4")
         }
         .padding()
     }
